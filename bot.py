63a077fb9c7ace115afa48e4f5ddd7d3cbe63077
Chandjaan <118669028+Chandjaan@users.noreply.github.com>
2022-12-18 02:47:12 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
-    Warrior = LazyDeveloper("@LazyDeveloper",
+    Warrior = LazyDeveloper("@chandjaan",
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
